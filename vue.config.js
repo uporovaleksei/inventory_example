e2459c575e631b1ea1ae40dd443a45601fa4b00c
Uporov Aleksei <uporovalekseii@yandex.ru>
2023-10-03 18:09:32 +0500

    final

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/inventory_example/" : "/",
-};
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/inventory_example/' : '/',
+})
